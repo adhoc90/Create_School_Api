e1175a718843b269c4ef2e14c422cbb0785733a3
ad_hoc <ad_hoc90@mail.ru>
2023-10-08 13:08:27 +0300

    Rest Tests final

--- src/test/java/ru/hogwarts/school/controller/TestConstants.java
+++ src/test/java/ru/hogwarts/school/controller/TestConstants.java
@@ -11,8 +11,7 @@ public class TestConstants {
     public static final Long MOCK_FACULTY_ID = 1L;
     public static final String MOCK_FACULTY_NAME = "Faculty name";
     public static final String MOCK_FACULTY_COLOR = "Faculty color";
-    public static final String OTHER_FACULTY_NAME = "Other faculty name";
-    public static final String OTHER_FACULTY_COLOR = "Other faculty color";
+
 
     public static final Faculty MOCK_FACULTY = new Faculty(
             MOCK_FACULTY_ID,
@@ -26,13 +25,11 @@ public class TestConstants {
     public static final String MOCK_STUDENT_NAME = "Student name";
     public static final Integer MOCK_STUDENT_AGE = 20;
     public static final Integer MOCK_STUDENT_AGE_TWO = 25;
-    public static final String OTHER_STUDENT_NAME = "Other student name";
+
     public static final Integer OTHER_STUDENT_AGE = 25;
     public static final Student MOCK_STUDENT = new Student(
             MOCK_STUDENT_ID,
             MOCK_STUDENT_NAME,
             MOCK_STUDENT_AGE
     );
-    public static final List<Student> MOCK_STUDENTS = Collections.singletonList(MOCK_STUDENT);
 }
-
